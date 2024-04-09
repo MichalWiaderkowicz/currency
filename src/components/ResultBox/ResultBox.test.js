81a521596fff658c13bd25d339c4ccb1a55c4366
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-09 19:28:20 +0200

    fix test

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -9,10 +9,10 @@ describe("Component ResultBox", () => {
 
   describe("should render proper info about conversion when PLN -> USD", () => {
     const testCases = [
-      { amount: "100", result: "PLN 100.00 = $28.57" },
-      { amount: "20", result: "PLN 20.00 = $5.71" },
-      { amount: "200", result: "PLN 200.00 = $57.14" },
-      { amount: "345", result: "PLN 345.00 = $98.57" },
+      { amount: 100, result: "PLN 100.00 = $28.57" },
+      { amount: 20, result: "PLN 20.00 = $5.71" },
+      { amount: 200, result: "PLN 200.00 = $57.14" },
+      { amount: 345, result: "PLN 345.00 = $98.57" },
     ];
 
     for (const testObj of testCases) {
@@ -42,10 +42,10 @@ describe("Component ResultBox", () => {
 
   describe("should render proper info about conversion when USD -> PLN", () => {
     const testCases = [
-      { amount: "100", result: "$100.00 = PLN 350.00" },
-      { amount: "20", result: "$20.00 = PLN 70.00" },
-      { amount: "200", result: "$200.00 = PLN 700.00" },
-      { amount: "345", result: "$345.00 = PLN 1,207.50" },
+      { amount: 100, result: "$100.00 = PLN 350.00" },
+      { amount: 20, result: "$20.00 = PLN 70.00" },
+      { amount: 200, result: "$200.00 = PLN 700.00" },
+      { amount: 345, result: "$345.00 = PLN 1,207.50" },
     ];
 
     for (const testObj of testCases) {
@@ -77,14 +77,14 @@ describe("Component ResultBox", () => {
   describe("should render proper info about conversion when from=to", () => {
     const testCases = [
       {
-        amount: "100",
+        amount: 100,
         from: "PLN",
         to: "PLN",
         result: "PLN 100.00 = PLN 100.00",
       },
-      { amount: "100", from: "USD", to: "USD", result: "$100.00 = $100.00" },
-      { amount: "25", from: "PLN", to: "PLN", result: "PLN 25.00 = PLN 25.00" },
-      { amount: "25", from: "USD", to: "USD", result: "$25.00 = $25.00" },
+      { amount: 100, from: "USD", to: "USD", result: "$100.00 = $100.00" },
+      { amount: 25, from: "PLN", to: "PLN", result: "PLN 25.00 = PLN 25.00" },
+      { amount: 25, from: "USD", to: "USD", result: "$25.00 = $25.00" },
     ];
 
     for (const testObj of testCases) {
@@ -112,10 +112,10 @@ describe("Component ResultBox", () => {
   });
   describe('should render "Wrong value..." when input < 0', () => {
     const testCases = [
-      { amount: "-100", from: "PLN", to: "PLN", result: "Wrong value..." },
-      { amount: "-10", from: "USD", to: "USD", result: "Wrong value..." },
-      { amount: "-240", from: "PLN", to: "USD", result: "Wrong value..." },
-      { amount: "-15", from: "USD", to: "PLN", result: "Wrong value..." },
+      { amount: -100, from: "PLN", to: "PLN", result: "Wrong value..." },
+      { amount: -10, from: "USD", to: "USD", result: "Wrong value..." },
+      { amount: -240, from: "PLN", to: "USD", result: "Wrong value..." },
+      { amount: -15, from: "USD", to: "PLN", result: "Wrong value..." },
     ];
 
     for (const testObj of testCases) {
